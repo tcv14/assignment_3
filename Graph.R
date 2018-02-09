405cf954a664c4a8b3b6387b227204952aed718d
xrlyu <xlyu@bu.edu>
2018-02-09 17:23:29 -0500

    Rough version, still need time series analysis

--- Graph.R
+++ Graph.R
@@ -65,21 +65,82 @@ sector_graph(PB2010,PB2017,'Big Bend')
 # Sector with most apprehensions in 2010
 PB2010$Total <- apply(PB2010[,-1],1,sum)
 most_2010 <- PB2010[PB2010$Total==max(PB2010$Total),]
+most_2010_s <- as.character(PB2010$Sector[PB2010$Total==max(PB2010$Total)]) # gives the name of the sector
 
 # Sector with most apprehensions in 2017
 PB2017$Total <- apply(PB2017[,-1],1,sum)
 most_2017 <- PB2017[PB2017$Total==max(PB2017$Total),]
+most_2017_s <- as.character(PB2017$Sector[PB2017$Total==max(PB2017$Total)]) # gives the name of the sector
 
-# Combine these two rows as a new matrix
-most <- t(rbind(most_2010,most_2017))
+# Combine these two rows as a new dataframe
+most <- as.data.frame(t(rbind(most_2010,most_2017)))
 colnames(most) <- c('2010','2017')
 most <- most[-1,]
 most <- most[-13,]
+most$`2010` <- as.numeric(as.character(most$`2010`))
+most$`2017` <- as.numeric(as.character(most$`2017`))
+
+# a variance test first
+var.test(most$`2010`,most$`2017`) # the result of the test is that two samples have same variance
+
+# then the initial t-test
+t.test(most$`2010`,most$`2017`,paired=FALSE,var.equal = TRUE) 
+# with a significance level of 0.05, we have to reject the null hypothesis and conclude that there are differences in mean
+
+# a second t-test to find out if the mean level of apprehension increases from 2010 to 2017
+t.test(most$`2010`,most$`2017`,paired=FALSE,var.equal = TRUE,alternative='greater')
+# with a significance level of 0.05, we have to reject the null hypothesis and conclude that the mean level in 2010 is greater than the mean level in 2017
+
 
 # Three months periods
 
-# By sector
+# write a function that would return a dataframe with the total number of apprehensions in 3-month period
+# object should be a data frame, particularly, should be either PB2010 or PB2017
 
+three_period <- function(object) {
+  period <- data.frame()
+  for (i in (1:nrow(object))) {
+    for (j in (1:(ncol(object)-3))){
+      period[i,j] <- object[i,j+1]+object[i,j+2]+object[i,j+3]
+    }
+  }
+  colnames(period) <- c('Oct-Dec','Nov-Jan','Dec-Feb','Jan-Mar','Feb-Apr','Mar-May','Apr-Jun',
+                        'May-Jul','Jun-Aug','Jul-Sep')
+  rownames(period) <- PB2017$Sector
+  period
+}
+
+t_month_2010 <- three_period(PB2010)
+t_month_2017 <- three_period(PB2017)
+
+# Calculate the total apprehension for each three month period and assign a name for the new row
+t_month_2010 <- rbind(t_month_2010,colSums(t_month_2010))
+rownames(t_month_2010) <- c(rownames(t_month_2010)[-length(rownames(t_month_2010))],'Total')
+
+t_month_2017 <- rbind(t_month_2017,colSums(t_month_2017))
+rownames(t_month_2017) <- c(rownames(t_month_2017)[-length(rownames(t_month_2017))],'Total')
+
+# Find the maximum apprehension level for each year and return the time period
+max_2010 <- t_month_2010[,t_month_2010[length(t_month_2010),]==max(t_month_2010[length(t_month_2010),])]
+max_2017 <- t_month_2017[,t_month_2017[length(t_month_2017),]==max(t_month_2017[length(t_month_2017),])]
+
+max_2010_period <- colnames(t_month_2010)[t_month_2010[length(t_month_2010),]==max(t_month_2010[length(t_month_2010),])] # gives the exact time period of the max level apprehension
+max_2017_period <- colnames(t_month_2017)[t_month_2017[length(t_month_2017),]==max(t_month_2017[length(t_month_2017),])] # gives the exact time period of the max level apprehension
+
+# Combine these two samples together under a new data frame
+max_three <- as.data.frame(cbind(max_2010,max_2017))
+
+# Perform a variance test first
+var.test(max_three$max_2010,max_three$max_2017) # the result of the variance test is that two samples having the same variance
+
+# then do a two-sided t-test
+t.test(max_three$max_2010,max_three$max_2017,paired = FALSE,var.equal = TRUE)
+# with a significance level of 0.05, we can conclude that means of these two samples are different
+
+# then do a one-sided test to see whether there is an increase in apprehension from 2010 to 2017
+t.test(max_three$max_2010,max_three$max_2017,paired=FALSE,var.equal = TRUE,alternative = 'less')
+# with a significance level of 0.05, we can conclude that mean level of apprehension in 2017 is greater than the mean level of apprehension in 2010
+# thus we can say that there is an increae in meal apprehension level from 2010 to 2017
 
 # # Time series data
 # ts <- as.vector(t(monthly[,-1]))
@@ -89,21 +150,6 @@ most <- most[-13,]
 # ts.plot(ts1)
 
 # monthly data mean
-monthly$mean <- apply(monthly[,-1],1,mean)
+# monthly$mean <- apply(monthly[,-1],1,mean)
 
 
-
-# three_period <- function(x) {
-#   period <- data.frame()
-#   for (i in (1:nrow(new_month))) {
-#     for (j in (1:(ncol(new_month)-2))){
-#       period[i,j] <- new_month[i,j]+new_month[i,j+1]+new_month[i,j+2]
-#     }
-#   }
-#   colnames(period) <- c('Jan-Mar','Feb-Apr','Mar-May','Apr-Jun','May-Jul','Jun-Aug','Jul-Sep',
-#                         'Aug-Oct','Sep-Nov','Oct-Dec')
-#   rownames(period) <- monthly$year
-#   period
-# }
-# 
-# three_period()
