9a1b7f69eb47bd48e1a1a8175c5b01858c31747d
xrlyu <xlyu@bu.edu>
2018-02-09 15:25:44 -0500

    02/09 update

--- Graph.R
+++ Graph.R
@@ -14,12 +14,15 @@ PB2017 <- cbind(Sector=PB2017[,1],PB2017[,5:ncol(PB2017)],PB2017[,2:4])
 monthly <- cbind(year=monthly[,1],monthly[,5:ncol(monthly)],monthly[,2:4])
 
 # Save cleaned data
-saveRDS(PB2010,file="PB Apprehensions 2010.rds")
-saveRDS(PB2017,file='PB Apprehensions 2017.rds')
-saveRDS(monthly,file='PB monthly summaries.rds')
-
+saveRDS(PB2010,file="./Data/PB Apprehensions 2010.rds")
+saveRDS(PB2017,file='./Data/PB Apprehensions 2017.rds')
+saveRDS(monthly,file='./Data/PB monthly summaries.rds')
 
 # Compare by month
+
+# write a function that would produce barplots by month
+# PB1 and PB2 should be dataframes, and the function is designed to let PB2010 = PB1 and PB2017 = PB2
+# n should be a string that indicates the month of the graph
 
 month_graph <- function(PB1,PB2,n){
     month <- c('January','Feburary','March','April','May','June','July','August',
@@ -28,7 +31,7 @@ month_graph <- function(PB1,PB2,n){
     m <- rbind(PB1[,time+1],PB2[,time+1])
     rownames(m) <- c('2010','2017')
     colnames(m) <- PB2010$Sector
-    barplot(as.matrix(m),col=c('darkblue','red'),xlab='Sector',ylab='Number of Apprehensions',
+    barplot(as.matrix(m),col=c('darkblue','red'),ylab='Number of Apprehensions',
             main=paste('Total Number of Illegal Alien Apprehensions in', n,sep = ' '),
             beside = TRUE,las=2,cex.names = 0.7)
     legend("topleft", 
@@ -36,9 +39,14 @@ month_graph <- function(PB1,PB2,n){
            fill = c("darkblue", "red"))
 }
 
+# an example of the function above
 month_graph(PB2010,PB2017,'January')
 
 # Compare by sector
+
+# write a function that would produce barplots by sector
+# PB1 and PB2 should be dataframes, and the functions is designed to take PB2010 as PB1 and PB2017 as PB2
+# n should be a string that indicates the sector of the graph
 
 sector_graph <- function(PB1,PB2,n){
     sector <- as.character(PB2010$Sector)
@@ -47,13 +55,15 @@ sector_graph <- function(PB1,PB2,n){
     rownames(m) <- c('2010','2017')
     colnames(m) <- c('January','Feburary','March','April','May','June','July','August',
                      'September','October','November','December')
-    barplot(as.matrix(m),col=c('yellow','green'),xlab='Month',ylab='Number of Apprehensions',
+    barplot(as.matrix(m),col=c('yellow','green'),ylab='Number of Apprehensions',
             main=paste('Total Number of Illegal Alien Apprehensions in',n,sep = ' '),
             beside = TRUE, las=2)
     legend("topright", 
            legend = rownames(m), 
            fill = c("yellow", "green"))
 }
+
+# an example of the function above
 
 sector_graph(PB2010,PB2017,'Big Bend')
 
@@ -67,11 +77,32 @@ most_2017 <- as.character(PB2017$Sector[PB2017$Total==max(PB2017$Total)])
 
 # Three months periods
 
-new_month <- subset(monthly[,-1])
+# By sector
 
-c('Jan-Mar','Feb-Apr','Mar-May','Apr-Jun','May-Jul','Jun-Aug','Jul-Sep','Aug-Oct','Sep-Nov','Oct-Dec')
-three <- function(x) {
-  for (i in (1:ncol(x)-2)) {
-    
-  }
-}
+
+# # Time series data
+# ts <- as.vector(t(monthly[,-1]))
+# ts3 <- ts(ts2, start = c(2000,10), frequency=12)
+
+
+# ts.plot(ts1)
+
+# monthly data mean
+monthly$mean <- apply(monthly[,-1],1,mean)
+
+
+
+# three_period <- function(x) {
+#   period <- data.frame()
+#   for (i in (1:nrow(new_month))) {
+#     for (j in (1:(ncol(new_month)-2))){
+#       period[i,j] <- new_month[i,j]+new_month[i,j+1]+new_month[i,j+2]
+#     }
+#   }
+#   colnames(period) <- c('Jan-Mar','Feb-Apr','Mar-May','Apr-Jun','May-Jul','Jun-Aug','Jul-Sep',
+#                         'Aug-Oct','Sep-Nov','Oct-Dec')
+#   rownames(period) <- monthly$year
+#   period
+# }
+# 
+# three_period()
