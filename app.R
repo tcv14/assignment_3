a1aa0f3405cc40e88d02533900df50ba8b974f06
xrlyu <xlyu@bu.edu>
2018-02-14 12:51:19 -0500

    New version 02/14, still need modifications

--- app.R
+++ app.R
@@ -13,53 +13,54 @@ library(shiny)
 data2010 <- readRDS('./Data/PB Apprehensions 2010.rds')
 data2017 <- readRDS('./Data/PB Apprehensions 2017.rds')
 monthlysummaries <- readRDS('./Data/PB monthly summaries.rds')
-colnames(monthlysummaries) <- c('Sector','October','November','December','January','February','March','April','May','June','July','August','September')
 
-completedata <- rbind(monthlysummaries,data2010,data2017)
+completedata <- rbind(data2010,data2017)
+
+# https://gist.github.com/aagarw30/c593799bc7d8557dc863411bb552e4f4
 
 # Define UI for application that draws a barplot
-ui <- navbarPage(
-  title = 'DataTable Options',
-  tabPanel('Display length',     DT::dataTableOutput('ex1')),
-  tabPanel('Length menu',        DT::dataTableOutput('ex2')),
-  tabPanel('No pagination',      DT::dataTableOutput('ex3')),
-  tabPanel('No filtering',       DT::dataTableOutput('ex4')),
-  tabPanel('Function callback',  DT::dataTableOutput('ex5'))
-)
-
-https://stackoverflow.com/questions/46145608/how-to-combine-top-navigation-navbarpage-and-a-sidebar-menu-sidebarmenu-in-s
-
-ui <- fluidPage(    
+ui <- pageWithSidebar(    
   
   # Give a title
-  titlePanel("Apprehensions by Region in 2010 and 2017"),
+  headerPanel("Apprehensions in 2010 and 2017"),
   
   # Generate a row with a sidebar
-  sidebarLayout(      
+  sidebarPanel(      
     
     # Define the sidebar with one input
-    sidebarPanel(
-      selectInput("sector", "Sector:", 
-                  choices=completedata$Sector)
+    conditionalPanel(
+      condition="input.tabselected==Comparison between 2010 and 2017",
+      selectInput("sector", "Sector:",choices=completedata$Sector)),
+    conditionalPanel(
+      condition="input.tabselected==By Year",
+      selectInput("year","Year:",choices=monthlysummaries$year))
+  ),
+  #    sidebarPanel(
+  #      selectInput("sector", "Sector:", 
+  #                  choices=completedata$Sector)
+  #    ),
+  
+  # Create a barplot
+  mainPanel(
+    tags$style(type="text/css",
+               ".shiny-output-error { visibility: hidden; }",
+               ".shiny-output-error:before { visibility: hidden; }"
     ),
-    
-    # Create a barplot
-    mainPanel(
-      tags$style(type="text/css",
-                 ".shiny-output-error { visibility: hidden; }",
-                 ".shiny-output-error:before { visibility: hidden; }"
-      ),
-      plotOutput("distPlot")
+    tabsetPanel(
+      id="tabselected",
+      tabPanel("Comparison between 2010 and 2017",value=1,plotOutput("distPlot1")),
+      tabPanel("By Year",value=2,plotOutput("distPlot2"))
     )
-    
+    #      plotOutput("distPlot")
   )
+  
 )
 
 # Define server logic required for a side by side barplot
 server <- function(input, output) {
   
   # Fill in the spot we created for a plot
-  output$distPlot <- renderPlot({
+  output$distPlot1 <- renderPlot({
     
     # Render a barplot
     barplot(height=as.matrix(completedata[completedata$Sector==input$sector,2:13]),
@@ -71,7 +72,14 @@ server <- function(input, output) {
             col = c("green","blue"))
     legend("topright",c("2010","2017"),pch=19,col=c("green","blue"),bty="n")
   })
+  output$distPlot2 <- renderPlot({
+    barplot(height=as.matrix(monthlysummaries[monthlysummaries$year==input$year,2:13]),
+            las=2,
+            main=imput$year,
+            ylab="Number of Apprehentions",
+            xlab="Year")
+  })
 }
 
-# Run the application 
+# Run the application and see what happens
 shinyApp(ui = ui, server = server)
