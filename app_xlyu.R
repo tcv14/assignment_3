7448b65ef2d563d6c652260438c56826dfd48316
xrlyu <xlyu@bu.edu>
2018-02-14 19:13:00 -0500

    still need modifications for conditional panel

--- app_xlyu.R
+++ app_xlyu.R
@@ -1,77 +1,57 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # load in the required data and rbind to create one data frame for side by side barplots later
 data2010 <- readRDS('./Data/PB Apprehensions 2010.rds')
 data2017 <- readRDS('./Data/PB Apprehensions 2017.rds')
 
-completedata <- rbind(data2010,data2017)
-completedata$year[1:9] <- '2010'
-completedata$year[10:18] <- '2017'
-
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-  headerPanel("Apprehensions in 2010 and 2017"),
+ui <- pageWithSidebar(
+  headerPanel("Comparison of Apprehensions in 2010 and 2017"),
   
-  sidebarPanel(      
-    
-    # Define the sidebar with one input
-    conditionalPanel(
-      condition="input.tabselected==By Sector",
-      selectInput("sector", "Sector:",choices=completedata$Sector)),
-    conditionalPanel(
-      condition="input.tabselected==By Year",
-      selectInput("month","Month:",choices=colnames(completedata)[-1]))
+  sidebarPanel(
+    conditionalPanel(condition='input.tabselected=="By Sector"',
+                     selectInput("sector", "Sector:",choices=data2010$Sector)),
+    conditionalPanel(condition ='input.tabselected="By Month"',
+                     selectInput("month", "Month:",choices=colnames(data2010)[-1]))
   ),
   
   mainPanel(
     tabsetPanel(
+      id='tabselected',
       tabPanel("By Sector",plotOutput("ex1")),
       tabPanel("By Month",plotOutput("ex2")))
   )
-  
-  
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
   output$ex1 <- renderPlot({
-    
-    # Render a barplot
     barplot(height = as.matrix(rbind(data2010[data2010$Sector==input$sector,2:13],
-                           data2017[data2017$Sector==input$sector,2:13])),
+                                     data2017[data2017$Sector==input$sector,2:13])),
             beside = TRUE,
             las=2,
             main=input$sector,
             ylab="Number of Apprehensions",
             xlab="Month",
-            col = c("green","blue"))
+            col = c("green","blue"),
+            cex.names = 0.8)
     legend("topright",c("2010","2017"),pch=19,col=c("green","blue"),bty="n")
   }
   )
   
   output$ex2 <- renderPlot({
-    matrix <- as.matrix(rbind(data2010[,which(colnames(completedata)==input$month)],
-                    data2017[,which(colnames(completedata)==input$month)]))
-    colnames(matrix) <- data2010$Sector
-      barplot(height= matrix,
+      barplot(height= as.matrix(rbind(data2010[,which(colnames(data2010)==input$month)],
+                                      data2017[,which(colnames(data2017)==input$month)])),
               beside = TRUE,
               las=2,
               names.arg = data2010$Sector,
               main=input$month,
-              ylab="Number of Apprehentions",
+              ylab="Number of Apprehensions",
               xlab="Sector",
-              col = c("green","blue"))
-    legend("topright",c("2010","2017"),pch=19,col=c("green","blue"),bty="n")
+              col = c("red","orange"),
+              cex.names=0.6)
+    legend("topright",c("2010","2017"),pch=19,col=c("red","orange"),bty="n")
     })
   }
 
